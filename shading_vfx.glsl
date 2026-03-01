af764727bd3d2233bab26e1c99d0906642e84c5c
eibertt <torreseiber153@gmail.com>
2026-03-01 18:31:16 -0400

    Create shading_vfx.glsl

---
+++ shading_vfx.glsl
@@ -0,0 +1,8 @@
+// TITAN B - SHADER CORE
+varying vec2 vUv;
+varying vec3 vNormal;
+void main() {
+    vUv = uv;
+    vNormal = normalize(normalMatrix * normal);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
